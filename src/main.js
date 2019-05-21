cf42f3c372b6520ed4b0a816540e0856bbc433e4
TarasTurchenko <tarasturchenko1551@gmail.com>
2019-05-21 19:53:06 +0300

    init

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+import './styles/index.scss';
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('[data-app]');
