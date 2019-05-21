d070e594f16c7d3d3aca5c3e97478425c123b383
Link001 <godzfm@gmail.com>
2019-05-21 20:07:16 +0300

    AboutSubject

---
+++ src/components/AboutSubject.vue
@@ -0,0 +1,13 @@
+<template>
+    <main>ABOUT SUBJECT</main>
+</template>
+
+<script>
+    export default {
+        name: "AboutSubject"
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import AboutSubject from './components/AboutSubject';
 
 Vue.use(Router);
 
 export default new Router({
-  routes: []
+  routes: [
+      {
+          path: '/about-subject',
+          component: AboutSubject
+      },
+  ]
 });
